0de92c53b1f813841d5c184607e61cfcbedf7882
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-12 19:48:42 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,4 @@
+######## Kullanıcıdan ismini alarak "Merhaba isim" yazan programı yazınız #########
+
+isim= input("İsminizi giriniz:")
+print("Merhaba {}" .format(isim))  # fazzzzzzzzzzzzla basic başladım :D
