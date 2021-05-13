78325b0fcaf2c45042f8a04811432eff88b5ec94
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-13 19:10:48 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,1 +1,6 @@
+########### 1'den 100'e kadar olan sayıların toplamını bulan program ###########
 
+# bu işlem için zaten matematiksel bir formül bulunmakta: n*(n+1)/2 
+
+n= int(100)
+print(int(n*(n+1)/2)) # cevabın sonunda ondalık 0'ı koymasını istemediğimden int'e çevirdim.
