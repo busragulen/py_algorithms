8984a57cbcd0aed7021fd5898eafd319d5802bbb
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-14 12:59:53 +0300

    Update diskriminant.py

--- diskriminant.py
+++ diskriminant.py
@@ -3,3 +3,9 @@
 # ax2 + bx +c=0 denklemin diskriminantı Δ =b2– 4ac ile bulunur.
 # kodu yazarken bu formülden yararlanmak yeterli olacaktır.
 
+a=int(input("Bir a degeri giriniz:"))
+b=int(input("Bir b degeri giriniz:"))
+c=int(input("Bir c degeri giriniz:"))
+d=b**2-4*a*c
+
+print("Girdiginiz degerlere gore ax2 + bx + c = 0 denkleminin deltasi:", d)
