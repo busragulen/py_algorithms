16bccc19498cbd04e799f320426be7fb7d094f0b
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-14 12:55:05 +0300

    Update and rename 9a.py to diskriminant.py

---
+++ diskriminant.py
@@ -0,0 +1,5 @@
+######### İkinci dereceden ax2 + bx + c = 0 denkleminin diskriminant çözümünü yapınız? Kullanıcıdan a,b ve c değerlerini alarak deltayı hesaplayınız ########
+
+# ax2 + bx +c=0 denklemin diskriminantı Δ =b2– 4ac ile bulunur.
+# kodu yazarken bu formülden yararlanmak yeterli olacaktır.
+
