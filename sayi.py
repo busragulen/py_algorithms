7174133675e234b14a12766351c417f14d32a70d
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-27 12:37:58 +0300

    Create sayi.py

---
+++ sayi.py
@@ -0,0 +1,9 @@
+#Klavyeden girilen bir sayının pozitif ya da negatif olduğunu ekrana yazdıran algoritmayı oluşturunuz.
+
+sayi=int(input("bir sayi girinz"))
+if sayi>0:
+    print("sayi pozitif")
+elif sayi==0:
+    print("sayi notr")
+else:
+    print("sayi negatif")
