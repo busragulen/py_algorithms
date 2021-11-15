2b4cf1e73d4e94f9d4f313720ec0bf80433bc59f
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-11-15 20:45:22 +0300

    Create km_to_mile.py

---
+++ km_to_mile.py
@@ -0,0 +1,10 @@
+# bu algoritma km-mil dönüşümü yapmaktadır
+# 1 km = 0,621371192 mil
+
+print("km-mil dönüştürücüsüne hoş geldiniz\n")
+
+km=float(input("km:"))
+mil=0.621371192*km
+
+print("girmiş olduğunuz km'nin mil cinsinden değeri:",mil)
+
