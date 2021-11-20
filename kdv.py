9b69c0b364ba1407d096973c67a1af6ed5799b93
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-11-20 10:49:07 +0300

    Update kdv.py

--- kdv.py
+++ kdv.py
@@ -1,5 +1,5 @@
 #Klavyeden girilen fiyatı, KDV(%18) ekleyerek ekrana yazdırın.
-print("KDV hesaplayiciya hodgeldiniz\n") # SFDHŞAFIOAHFA krala bak be
+print("KDV hesaplayiciya hodgeldiniz\n")
 a= int(input("Bir fiyat giriniz:"))
 
 print("Verdiğiniz fiyatın KDV(%18) eklenmis hali:", a+a*18/100)
